99893a8c36a9281f7309ab47558543df10305d6e
Nagendra Varma <155356298+varma027@users.noreply.github.com>
2026-05-27 17:47:23 +0530

    Add files via upload

---
+++ DAY-2/keywords.py
@@ -0,0 +1,8 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> import keyword
+>>> 
+>>> print (keyword.kwlist)
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+>>> print(len(keyword.kwlist))
+35
