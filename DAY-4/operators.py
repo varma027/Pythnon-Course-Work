09eadb3cd1103db234541a9cb3237030f21476c0
Nagendra Varma <155356298+varma027@users.noreply.github.com>
2026-06-11 12:10:08 +0530

    Add files via upload

---
+++ DAY-4/operators.py
@@ -0,0 +1,186 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+'''
+arthimetic operator
+'''
+'\narthimetic operator\n'
+a = 10
+b = 20
+
+a+b
+30
+a-b
+-10
+a*b
+200
+a/b
+0.5
+a//b
+0
+a*
+SyntaxError: invalid syntax
+a**2
+100
+a%b
+10
+'''
+comparision operator
+'''
+'\ncomparision operator\n'
+
+a<b
+True
+a>b
+False
+a=b
+a<=b
+True
+a>=b
+True
+34>b
+True
+34<a
+False
+a==b
+True
+a!=b
+False
+a!=b
+False
+a=10
+b=10
+a!=b
+False
+is a=b
+SyntaxError: invalid syntax
+
+                                
+a = 5
+a
+5
+a +=5
+a
+10
+a *=5
+a
+50
+a
+50
+a
+50
+a
+
+a
+
+a
+
+a
+
+a
+
+a
+50
+a
+
+a
+a
+50
+a
+
+a
+50
+a
+
+a
+50
+a
+
+a
+50
+a
+
+a
+50
+a
+
+a
+
+a
+50
+a
+
+type(a)
+<class 'int'>
+float(a)
+50.0
+
+
+'''
+assignment operator
+'''
+'\nassignment operator\n'
+
+'''
+and or not are addignment operators
+'''
+'\nand or not are addignment operators\n'
+#membership operator in, not in
+a = 'python programming'
+'p' in a
+True
+'g in a'
+'g in a'
+'g ' in a
+False
+'sa' in a
+False
+l = ['dsxfsdgs','g','fsd','rfesd','gf','sdg','g','sg']
+>>> 'rfesd' in l
+True
+>>> 
+>>> l = [1,2,3,4,5,6]
+>>> m = [1,2,3,4,5,6]
+>>> 
+>>> m = n
+Traceback (most recent call last):
+  File "<pyshell#85>", line 1, in <module>
+    m = n
+NameError: name 'n' is not defined
+>>> n = m
+>>> l
+[1, 2, 3, 4, 5, 6]
+>>> id(l)
+1703342164928
+>>> id
+<built-in function id>
+(
+>>> id(m)
+1703386108288
+>>> id(n)
+1703386108288
+>>> #both n and m are pointing towards the same memory reference
+>>> l is not m
+True
+>>> l is m
+False
+>>> l is n
+False
+>>> l is not n
+True
+>>> n is not m
+False
+>>> n is m
+True
+>>> '''subbu'''
+'subbu'
+>>> del 'subbu'
+SyntaxError: cannot delete literal
+>>> '''
+... left shift means adding 0 to the right
+... right shift means removing bights from the rihgt side
+... '''
+'\nleft shift means adding 0 to the right\nright shift means removing bights from the rihgt side\n'
+>>> 9959974411
+9959974411
+>>> del 9959974411
+SyntaxError: cannot delete literal
